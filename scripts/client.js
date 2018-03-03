05b63a917cf11b51557634506b73d9bcd52b2b90
Josh Nothum <joshnothum@gmail.com>
2018-03-03 01:50:29 -0600

    changed size of blackKeys tile

--- scripts/client.js
+++ scripts/client.js
@@ -1,22 +1,22 @@
 let myApp = angular.module('myApp', []);
 
-myApp.controller('HomeController',['$location', function () {
+myApp.controller('HomeController', ['$location', function () {
     let home = this;
     home.message = 'Hello!';
-console.log('loaded');
+    console.log('loaded');
 
 
-home.blogDirect = function(){
-    window.location.href = "https://www.joshnothum.com";
-   };
-   home.soloDirect = function () {
-       window.location.href = "https://glacial-sierra-30356.herokuapp.com/#/home";
-   };
+    home.blogDirect = function () {
+        window.location.href = "https://www.joshnothum.com";
+    };
+    home.soloDirect = function () {
+        window.location.href = "https://glacial-sierra-30356.herokuapp.com/#/home";
+    };
 
-   home.groupDirect = function () {
-       window.location.href = "https://github.com/AndrewResidence/andrew-residence";
-   };
-home.blackDirect = function () {
-    window.location.href = "https://github.com/joshnothum/black_keys_analysis";
-};
+    home.groupDirect = function () {
+        window.location.href = "https://github.com/AndrewResidence/andrew-residence";
+    };
+    home.blackDirect = function () {
+        window.location.href = "https://github.com/joshnothum/black_keys_analysis";
+    };
 }]);
